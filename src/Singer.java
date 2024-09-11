74b32d956fc8609a2d009063a47bcef00c02093b
Harry Lawal <harrylawal74@gmail.com>
2024-09-11 19:17:46 +0100

    cleaning up code

--- src/Singer.java
+++ src/Singer.java
@@ -1,42 +1,59 @@
-public class Singer {
+public class Singer
+{
     boolean happy;
 
-    public Singer() {
+    public Singer()
+    {
         happy = true;
     }
 
-    public void sing() {
+    public void sing()
+    {
         if (happy == true) {
             System.out.println("Things can only get better");
-        } else {
+        }
+        else {
             System.out.println("It's all completely pointless");
         }
     }
 
-    public void sing1() {
+    public void sing1()
+    {
         if (happy) {
             System.out.println("Things can only get better");
-        } else {
+        }
+        else {
             System.out.println("It's all completely pointless");
         }
     }
 
-    public boolean isSad() {
-        if (happy == true) {
-            return false;
-        } else {
-            return true;
+    public boolean isSad()
+    {
+        // "!" returns opposite of logical state of operand e.g. if "happy" is true then returns false.
+        return !happy;
+    }
+
+    public boolean isHappy()
+    {
+        //returns the double opposite of logical state of operand
+        return !(!happy);
+    }
+
+    public void changeMood() {
+        if(happy == true) {
+            happy = false;
+        }
+        else
+        {
+            happy = true;
         }
     }
 
-    public boolean badIsSad() {
-        if (happy = true) {
-            return false;
-        } else {
-            return true;
-        }
+    public static void main(String[] args) {
+        Singer person1 = new Singer();
+
+
+
+        System.out.println(person1.isHappy());
     }
 }
-
-
-
